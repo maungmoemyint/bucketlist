e0e0cf4e97ef8a7a2cd43dc9679e273eb1ec6356
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-07-28 18:03:46 +0200

    tasks of setting up capybara

--- test/system/using_capybara_test.rb
+++ test/system/using_capybara_test.rb
@@ -14,4 +14,17 @@ class CapybaraTest < ApplicationSystemTestCase
     click_on('Manarola', match: :first)
     sleep(20.seconds)
   end
+
+  test 'we can use capybara' do
+    visit('https://en.wikipedia.org/wiki/Internet')
+    sleep(5.seconds)
+    fill_in('searchInput', with: 'Ruby Programming language')
+    sleep(5.seconds)
+    click_on('searchButton', match: :first)
+    sleep(5.seconds)
+    click_on('Examples', match: :first)
+    sleep(5.seconds)
+    click_on('Hello world', match: :first)
+    sleep(5.seconds)
+  end
 end
