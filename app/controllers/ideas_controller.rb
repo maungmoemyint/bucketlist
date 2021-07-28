5579af77f3443c077425a83ff97e98a73c358d4e
maungmoemyint <maungmoe.myint@aya.yale.edu>
2021-07-28 17:10:05 +0200

    Create first test script

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -12,8 +12,23 @@ class IdeasController < ApplicationController
     idea = Idea.new
     idea.title = params[:title]
     idea.done_count = params[:done_count]
-    idea.photo_url = params[:photo__url]
+    idea.photo_url = params[:photo_url]
     idea.save!
     redirect_to ideas_index_path
   end
+
+  def edit
+    id = params[:id]
+    @idea = Idea.find(id)
+  end
+
+  def update
+    idea = Idea.find (params[:id])
+    idea.title = params[:title]
+    idea.done_count = params[:done_count]
+    idea.photo_url = params[:photo_url]
+    idea.save!
+    redirect_to account_ideas_path
+  end
+
 end
